a6cf97ce6212c035a4353e05cfe5f136930a8a77
nearbyapapermacbook <Wuttipat_tua@truecorp.co.th>
2024-05-21 23:20:32 +0700

    clear unuse reducer

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
-import usersReducer from './user/userSlice';
 import {counterReducer} from './reducers/counter-reducer';
 
 const store = configureStore({
   reducer: {
-    users: usersReducer,
     counter: counterReducer,
   },
 });
